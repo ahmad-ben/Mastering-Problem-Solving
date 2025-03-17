2af55269184c3d4670d3abd86d839aa2f95b3881
ahmad-ben <ahmedbenchakhter@gmail.com>
2025-03-17 23:45:49 +0000

    Solve the 605. Can Place Flowers problem.

---
+++ LeetCode/Random Problems/Easy/605. Can Place Flowers/Solutions/my-solution.js
@@ -0,0 +1,10 @@
+var canPlaceFlowers = function(flowerbed, n) {
+  for(let i = 0; i < flowerbed.length; ++i){
+    if(
+      flowerbed[i - 1] === 1 || flowerbed[i] === 1 || flowerbed[i + 1] === 1
+    ) continue;
+    flowerbed[i] = 1, --n;
+  };
+
+  return n <= 0;
+};
